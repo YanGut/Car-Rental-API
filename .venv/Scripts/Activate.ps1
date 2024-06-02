70ffb212c819a569e2e1a3fd95b1aaa8fd4813aa
FagTI <guttlauncher@gmail.com>
2024-06-02 15:45:59 -0300

    add a virtual environment .venv

---
+++ .venv/Scripts/Activate.ps1
@@ -0,0 +1,7 @@
+      PID    PPID    PGID     WINPID   TTY         UID    STIME COMMAND
+      953       1     953      16796  cons4     197609 15:21:59 /usr/bin/bash
+      833       1     833      10668  cons1     197609 15:19:16 /usr/bin/bash
+     1212     881    1212      16724  cons3     197609 15:40:40 /usr/bin/PS
+      881       1     881      18724  cons3     197609 15:20:23 /usr/bin/bash
+      836       1     836       5520  cons2     197609 15:19:17 /usr/bin/bash
+     1115       1    1115      16480  cons5     197609 15:34:32 /usr/bin/bash
